13be04d692618dac05e415e032678b9808dea6d1
boom <jeanmar-97.1@hotmail.fr>
2021-08-29 00:10:04 +0200

    changement de font, de couleur de la barre nav, des cartes, ajout du logo au panier via font awesome

--- Frontend/orinoco.js
+++ Frontend/orinoco.js
@@ -3,13 +3,12 @@ fetch("http://localhost:3000/api/cameras")
     .then(response => response.json()
         .then(data => {
             console.log(data);
-            let affichage = `<ul style="display:flex; flex-wrap:wrap; justify-content:center; align-items : center;">`;
+            let affichage = `<ul style="display:flex; flex-wrap:wrap; margin:auto; align-items : center;justify-content:center;">`;
             for (let dat of data) {
-                affichage += `<div class="card" style="width: 22rem;">
+                affichage += `<div class="card" style="width: 21rem;">
                 <img class="card-img-top" src="${dat.imageUrl}" alt="Appareil photo">
                 <div class="card-body">
                   <p style="font-weight: bold" class="card-text">${"Modèle :"+ " "+dat.name}</p>
-                  <p style="font-weight: bold" class="card-text">${"Prix :"+ " "+dat.price + " " + "€"}</p>
                 </div>
               </div>`
             }
