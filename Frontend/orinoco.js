40d48a0171c1f6e37ac25f313d3e34f15322871d
boom <jeanmar-97.1@hotmail.fr>
2021-08-29 14:36:41 +0200

    ajout d'un jquery permettant d'ajouter un evenement sur plusieurs class à la fois ( entre autre le bouton commander)

--- Frontend/orinoco.js
+++ Frontend/orinoco.js
@@ -1,21 +1,30 @@
 /* TEST récupération des informations de l'api et affichage de resultat en html*/
 fetch("http://localhost:3000/api/cameras")
-    .then(response => response.json()
-        .then(data => {
-            console.log(data);
-            let affichage = `<ul style="display:flex; flex-wrap:wrap; margin:auto; align-items : center;justify-content:center;">`;
-            for (let dat of data) {
-                affichage += `<div class="card" style="width: 21rem;">
+  .then(response => response.json()
+    .then(data => {
+      console.log(data);
+      let affichage = `<ul style="display:flex; flex-wrap:wrap; margin:auto; align-items : center;justify-content:center;">`;
+      for (let dat of data) {
+        affichage += `<div class="card" style="width: 21rem;">
                 <img class="card-img-top" src="${dat.imageUrl}" alt="Appareil photo">
                 <div class="card-body">
-                  <p style="font-weight: bold" class="card-text">${"Modèle :"+ " "+dat.name}</p>
+                  <p style="font-weight: bold" class="card-text">${"Modèle :" + " " + dat.name}</p>
+                  <button  type="submit" class="btn btn-primary btn-sm touch ">Commander</button>
                 </div>
-              </div>`
-            }
-            affichage += '</ul>';
-            document.getElementById("mouv").innerHTML= affichage  
-        }))
-
+              </div>`;
+      }
+      affichage += '</ul>';
+      document.getElementById("mouv").innerHTML = affichage;
+      
+      //clique sur commander pour aller à la page produit//
+      document.querySelectorAll('.touch').forEach(item => {
+        item.addEventListener('click', event => {
+          console.log("tu clique"); window.location.assign("produit.html")
+        })
+      })
+    }));
+    
+    
 
 
 
