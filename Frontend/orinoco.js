16b9f1420f44e6f8a1c77909da41f5274679d217
boom <jeanmar-97.1@hotmail.fr>
2021-09-10 15:10:42 +0200

    déblocage grâce à Marc, affichage des produits selectionné dans le panier, boucle allstorage() , onload sur page panier, dans cette boucle on creer une boucle fetch

--- Frontend/orinoco.js
+++ Frontend/orinoco.js
@@ -33,45 +33,61 @@ fetch(`http://localhost:3000/api/cameras/${id}`)
   .then(resultat => resultat.json()
     .then(data2 => {
       console.log(data2);
+      let Displaylenses
       let affichage2 = `<div class="card" style="margin-right:auto; margin-left :auto;width: 18rem;">
       <img class="card-img-top" src="${data2.imageUrl}" alt="Appareil photo">
       <div style="display:flex;justify-content-center;flex-direction:column" class="card-body">
         <p style="text-align:center;font-weight: bold;" class="card-text"><span>Modèle :</span>${" " + " " + data2.name}</p>
         <p style="text-align:center;font-weight: bold" class="card-text"><span>Prix :</span>${" " + (data2.price / 100) + " " + "€"}</p>
         <label style="text-align:center;margin-bottom:20px;font-weight: bold" for="lense-select">Lentilles</label>
-        <select " class="form-group id="lense-select">
-  <option>${data2.lenses[0]}</option>
-  <option>${data2.lenses[1]}</option>
-  <option>${data2.lenses[2]}</option>
+        <select  class="form-group" id="lense-select">
+  
 </select>
-        <button id=${data2.id}  type="button" onClick="isclicked(id),allstorage()"   class="btn btn-primary btn-sm touch ">Selectionner</button>
+        <button id=${data2.id}  type="button" onClick="isclicked(id)"   class="btn btn-primary btn-sm touch ">Selectionner</button>
         <button style="margin-top:10px" id=${data2._id}  type="submit" onclick="DeleletItem()" class="btn btn-primary btn-sm touch ">Supprimer du panier</button>
       </div>
     </div>`;
-
+      //boucle pour les lentilles//
+      for (let lense of (data2.lenses)) {
+        Displaylenses += `<option value="${lense}">${lense}</option>`
+      };
       document.getElementById("mouv2").innerHTML = affichage2;
+      document.getElementById("lense-select").innerHTML = Displaylenses;
     }));
-//panier//
-const panier = []
-//supprimer produit du localstorage//
-function DeleletItem() {
-  localStorage.removeItem(id);
-  console.log(panier)
-}
+
 
 //choisir produit//
 function isclicked() {
   localStorage.setItem(id, id);
   alert('vous avez choisi ce produit')
 }
-//recuperer le localstorage et le mettre dans un tableau//
-function allstorage() {
-  for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    panier.push(key)
-    console.log(panier);
+//recuperer le localstorage en tableau sur la page panier//
+function allStorage() {
+
+  var values = [];
+  let AffichageCamera = `<ul style="display:flex; flex-wrap:wrap; margin:auto; align-items : center;justify-content:center;">`;
+  for (var i = 0; i < localStorage.length; i++) {
+    values.push(localStorage.getItem(localStorage.key(i)));
+
+    fetch(`http://localhost:3000/api/cameras/${values[i]}`)
+      .then(Resultatcamera => Resultatcamera.json()
+        .then(cameras => {
+          console.log("panier :"+ cameras.name);
+          
+          AffichageCamera += `<li   class="card cardtransfo" style="width: 21rem;">
+          <img class="card-img-top" src="${cameras.imageUrl}" alt="Appareil photo">
+          <a href="produit.html?article=${cameras._id}" style="text-decoration:none" class=" stretched-link card-body">
+            <p style="font-weight: bold" class="card-text">${"Modèle :" + " " + cameras.name}</p>
+          </a>
+        </li>`;
+
+        AffichageCamera += '</ul>';
+        document.getElementById("mouv3").innerHTML = AffichageCamera;
+        }));
+
   }
+  
+
+  return values;
+
 }
-
-
-
